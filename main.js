1321889768af424f4a669833a59fcb61dfaba695
Barbie-6 <83456139+Barbie-6@users.noreply.github.com>
2022-01-21 18:03:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(640, 490);
+    canvas.position(480, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+     image(video, 230, 150, 220, 200);
+     fill("green");
+     rect(100, 100, 460, 20);
+     rect(100, 430, 460, 20);
+     rect(100, 120, 20, 320);
+     rect(550, 120, 20, 320);
+     fill("red");
+     circle(110, 110, 50);
+     circle(110, 440, 50);
+     circle(560, 110, 50);
+     circle(560, 440, 50);
+     
+}
